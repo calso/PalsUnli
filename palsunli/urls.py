df01cb66c4d9e6f6778eb6dcb952dec10fcfc463
Eman Calso <egcalso@gmail.com>
2011-09-03 19:46:56 +0800

    Add profiles url

--- palsunli/urls.py
+++ palsunli/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^registration/', include('registration.urls')),
+    url(r'^profiles/', include('profiles.urls')),
     url(r'^accounts/', include('accounts.urls')),
 )
