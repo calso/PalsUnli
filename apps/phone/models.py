dab142614c4e517b547e92913407ddb7ef09a988
Katrina Calso <k@palsunli.com>
2011-09-10 19:06:22 +0800

    create phone and network models

--- apps/phone/models.py
+++ apps/phone/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Network(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    
+
+class Phone(models.Model):
+    user = models.ForeignKey(User)
+    number = models.CharField(max_length=16, unique=True)
+    network = models.ForeignKey(Network)
