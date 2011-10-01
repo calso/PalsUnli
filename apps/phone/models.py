0a571003382aadff99319f46be0bd230fbcfbf0d
Eman Calso <eman@palsunli.com>
2011-10-01 21:03:16 +0800

    Make change the number to short form when saving.

--- apps/phone/models.py
+++ apps/phone/models.py
@@ -15,4 +15,18 @@ class Phone(models.Model):
         """
         Override default save method
         """
+        self.number = self.get_short_number()
         return super(Phone, self).save(**kw)
+
+    def get_short_number(self):
+        value = self.number
+        if value.startswith('0') or \
+            value.startswith('+') or \
+            ' ' in value or \
+            '-' in value or \
+            '.' in value:
+            # Remove spaces, dashes and dots
+            value = value.replace(' ', '').replace('-', '').replace('.', '')
+            # Remove prefix 0 or country code (just Philippines or +63 for now)
+            value = value.lstrip('+63').lstrip('0')
+        return value
